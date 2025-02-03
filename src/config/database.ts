8ff5d65a31d85310eed7ed5398c45a8e248c51bd
Habtemariam <habteshbaki2014@gmail.com>
2025-02-02 22:36:05 -0800

    I made some change for handle errors

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-// Create an instance of PrismaClient
 const prisma = new PrismaClient();
-
 export default prisma;
